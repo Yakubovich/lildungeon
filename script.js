2714dcb57ee5fea84d6f35416f1d37787600f95a
Ilya Yakubovich <ilya.yakubovich@gmail.com>
2012-06-20 11:54:30 -0400

    Moved nested functions to prototypes to increase performance

--- script.js
+++ script.js
@@ -39,11 +39,6 @@ $(document).ready(function() {
 
   /* Initialize walls */
   sprites.push(new Wall({ width: 200, height: 200, xPos: 200, yPos: 200 }));
-  /*
-  sprites.push(new Wall({ width: 400, height: 200, xPos: 300, yPos: 100 }));
-  sprites.push(new Wall({ width: 400, height: 100, xPos: 0, yPos: 400 }));
-  sprites.push(new Wall({ width: 30, height: 400, xPos: 0, yPos: 0 }));
-  */
 
   /* Place some sprites in non-colliding positions around the map */
   var guy;
@@ -58,6 +53,8 @@ $(document).ready(function() {
                          xPos:   Math.round(Math.random() * canvasWidth),
                          yPos:   Math.round(Math.random() * canvasHeight)
                        });
+
+      /* Turn one of the sprites into a zombie */
       if (i == 0) {
         guy.type = ZOMBIE;
         guy.speed = 2;
@@ -65,6 +62,7 @@ $(document).ready(function() {
         guy.image.src = "ch-zombie.png";
       }
 
+      /* Make sure that the sprite isn't hitting another sprite */
       for (var i2 = 0; i2 < sprites.length; i2++) {
         if (hitTest(guy, sprites[i2])) {
           hit = true;
@@ -75,18 +73,6 @@ $(document).ready(function() {
     sprites.push(guy);
   }
 
-  
-  /*
-  wolf = new Sprite({ src:         "wolf.png", 
-                      width:       38,
-                      height:      39,
-                      xPos:        Math.round(Math.random() * canvasWidth), 
-                      yPos:        Math.round(Math.random() * canvasHeight),
-                      totalframes: 3,
-                      speed:       1
-                    });
-  sprites.push(wolf);
-  */
 
   /* Start draw loop */
   setInterval(draw,50);
@@ -94,7 +80,12 @@ $(document).ready(function() {
   ctx = $('#game')[0].getContext('2d');
 });
 
-
+Function.prototype.inheritsFrom = function (parentClass) {
+  this.prototype = new parentClass();
+  this.prototype.constructor = this;
+  this.prototype.parent = parentClass.prototype;
+  return this;
+}
 
 /* Returns true if r1 and r2 are overlapping */
 function hitTest (r1, r2) {
@@ -107,16 +98,17 @@ function hitTest (r1, r2) {
      return false;
 }
 
+/* Like Math.round(num), but faster */
 function quickRound (num) {
   return (0.5 + num) << 0;
 }
-
 
 /* The draw loop */
 function draw() {
   ctx.clearRect(0,0,500,500);
   var theSprite, theOtherSprite;
 
+  /* There's a 0.5% chance for a shotgun to be placed on the map, unless already present */
   if (Math.random() > 0.995 && !shotgunPresent) {
     var shotgun = new Item({ type: SHOTGUN, src: "shotgun.png", width: 24, height: 7, xPos: Math.random() * canvasWidth, yPos: Math.random() * canvasHeight });
     sprites.push(shotgun);
@@ -132,14 +124,17 @@ function draw() {
     });
   }
 
+  /* Iterate through the sprites */
   for (var index = 0; index < sprites.length; index++) {
 
     theSprite = sprites[index];
     
+    /* Change direction */
     if (theSprite != player && Math.random() > theSprite.turnProb && theSprite.type != DEAD) {
       theSprite.randomDir();
     }
 
+    /* Don't run off the map */
     if (theSprite.position.x + theSprite.width >= canvasWidth)
       theSprite.direction = LEFT;
     if (theSprite.position.x <= 0)
@@ -209,28 +204,33 @@ function draw() {
   });
 }
 
+/* THE POSITION CLASS */
+/* Represents a point on the canvas */
+/* Required: x and y float coordinates */
 function Position (x, y) {
   this.x = x;
   this.y = y;
   this.position = this;
-
-  this.add = function(p2) {
-    this.x += p2.x;
-    this.y += p2.y; 
-  };
-
-  this.subtract = function(p2) {
-    this.x -= p2.x;
-    this.y -= p2.y; 
-  };
-
-  this.distanceTo = function(p2) {
-    var dx = this.x - p2.x;
-    var dy = this.y - p2.y;
-    return Math.sqrt((dx * dx) + (dy * dy));
-  }; 
 }
 
+Position.prototype.add = function(p2) {
+  this.x += p2.x;
+  this.y += p2.y; 
+}
+
+Position.prototype.subtract = function(p2) {
+  this.x -= p2.x;
+  this.y -= p2.y; 
+}
+
+Position.distanceTo = function(p2) {
+  var dx = this.x - p2.x;
+  var dy = this.y - p2.y;
+  return Math.sqrt((dx * dx) + (dy * dy));
+}
+
+/* THE ITEM CLASS */
+/* Represents a non-animated item that can be rendered on the canvas (e.g. a weapon) */
 function Item (args) {
   this.type = args.type;
   this.position = new Position(args.xPos, args.yPos);
@@ -239,21 +239,27 @@ function Item (args) {
   this.image = new Image ();
   this.image.src = args.src;
   this.image.loaded = true;
-  this.nextFrame = function () {
-    ctx.drawImage(this.image,
-                  0,
-                  0,
-                  this.width,
-                  this.height,
-                  this.position.x,
-                  this.position.y,
-                  this.width,
-                  this.height);
-  }
-  this.reverse = function () {};
-  this.zombify = function () {};
 }
 
+Item.prototype.nextFrame = function() {
+  ctx.drawImage(this.image,
+                0,
+                0,
+                this.width,
+                this.height,
+                this.position.x,
+                this.position.y,
+                this.width,
+                this.height);
+}
+
+Item.prototype.reverse = function() {};
+Item.prototype.zombify = function() {};
+
+
+/* THE WALL CLASS */
+/* Represents a rectangular, stationary wall that can be rendred on the canvas */
+/* Required: xPos, yPos, width, height */
 function Wall (args) {
   this.type = WALL;
   this.position = new Position(args.xPos, args.yPos);
@@ -261,14 +267,20 @@ function Wall (args) {
   this.height = args.height;
   this.image = new Object ();
   this.image.loaded = true;
-  this.nextFrame = function () {
-    ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
-  };
   this.reverse = function () {};
   this.zombify = function () {};
 }
 
-/* Requied args: src, width, height, xPos, yPos */
+Wall.prototype.nextFrame = function() {
+  ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+};
+
+Wall.prototype.reverse = function() {};
+Wall.prototype.zombify = function() {};
+
+
+/* THE SPRITE CLASS */
+/* Requied args:  src, width, height, xPos, yPos */
 /* Optional args: turnprob, totalframes, stop, speed */
 function Sprite(args) {
   this.frame = 0;
@@ -310,172 +322,167 @@ function Sprite(args) {
     this.loaded = true;        
   };
 
-  this.zombify = function() {
-    if (this.type != DEAD) {
-      this.type = ZOMBIE;
-      this.speed = 2;
-      this.image.src = "ch-zombie.png";
-    }
-  };
-
-  this.goTo = function(target) {
-    var delta = new Position(target.x, target.y);
-    delta.subtract(this.position);
-    var angle = Math.atan2(delta.y, delta.x);
-    this.movement.x = Math.cos(angle) * this.speed;
-    this.movement.y = Math.sin(angle) * this.speed;
-    
-    if (angle > 0) {
-      if (angle > 3 * Math.PI / 4)
-        this.direction = LEFT;
-      else
-        this.direction = DOWN;
-    } else {
-      if (angle > - Math.PI / 4)
-        this.direction = RIGHT;
-      else
-        this.direction = UP;
-    }
-  };
-
-  this.randomDir = function() {
-    var random = Math.ceil(Math.random() * 4);
-    switch(random){
-      case 1:
-        this.direction = UP;
-        break;
-      case 2:
-        this.direction = DOWN;
-        break;
-      case 3:
-        this.direction = LEFT;
-        break;
-      case 4:
-        this.direction = RIGHT
-        break;
-    }
-    this.frustration--;
-  };
-
-  /* Call this a sprite is intialized */
   this.randomDir();
+};
 
-  this.reverse = function() {
-    if (this != player) {
+Sprite.prototype.zombify = function() {
+  if (this.type != DEAD) {
+    this.type = ZOMBIE;
+    this.speed = 2;
+    this.image.src = "ch-zombie.png";
+  }
+}
+
+Sprite.prototype.goTo = function(target) {
+  var delta = new Position(target.x, target.y);
+  delta.subtract(this.position);
+  var angle = Math.atan2(delta.y, delta.x);
+  this.movement.x = Math.cos(angle) * this.speed;
+  this.movement.y = Math.sin(angle) * this.speed;
+  
+  if (angle > 0) {
+    if (angle > 3 * Math.PI / 4)
+      this.direction = LEFT;
+    else
+      this.direction = DOWN;
+  } else {
+    if (angle > - Math.PI / 4)
+      this.direction = RIGHT;
+    else
+      this.direction = UP;
+  }
+}
+
+Sprite.prototype.randomDir = function() {
+  var random = Math.ceil(Math.random() * 4);
+  switch(random){
+    case 1:
+      this.direction = UP;
+      break;
+    case 2:
+      this.direction = DOWN;
+      break;
+    case 3:
+      this.direction = LEFT;
+      break;
+    case 4:
+      this.direction = RIGHT
+      break;
+  }
+  this.frustration--;
+}
+
+Sprite.prototype.reverse = function() {
+  if (this != player) {
+    switch(this.direction) {
+      case UP:
+        this.direction = DOWN;
+        break;
+      case DOWN:
+        this.direction = UP;
+        break;
+      case LEFT:
+        this.direction = RIGHT;
+        break;
+      case RIGHT:
+        this.direction = LEFT;
+        break;
+    }
+  } else {
+    this.stop = true;
+  }
+
+  this.xPos = this.xPosOld;
+  this.yPos = this.yPosOld;
+  this.position.x = this.oldPosition.x;
+  this.position.y = this.oldPosition.y;
+}
+
+Sprite.prototype.drawFrame = function(row, col) {
+  if (row >= 0) {
+    ctx.drawImage(this.image, 
+                  this.width * col,
+                  row * this.height,
+                  this.width,
+                  this.height,
+                  quickRound(this.position.x),
+                  quickRound(this.position.y),
+                  this.width,
+                  this.height); 
+  } else {
+    ctx.save();
+    ctx.scale(-1,1);
+    row = -row;
+    ctx.drawImage(this.image,
+                  this.width * col,
+                  row * this.height,
+                  this.width,
+                  this.height,
+                  -quickRound(this.position.x) - this.width,
+                  quickRound(this.position.y),
+                  this.width,
+                  this.height); 
+    ctx.restore();
+  }
+}
+
+Sprite.prototype.nextFrame = function() {
+  this.xPosOld = this.xPos;
+  this.yPosOld = this.yPos;
+
+  this.oldPosition.x = this.position.x;
+  this.oldPosition.y = this.position.y;
+
+  this.movement.x = 0;
+  this.movement.y = 0;
+
+  if (this.type == DEAD) {
+    //temporary, will be part of sprite
+    var img = new Image();
+    img.src = "dead-zombie.png";
+    ctx.drawImage(img, 
+                  0,
+                  0,
+                  22,
+                  10,
+                  this.position.x,
+                  this.position.y + 10,
+                  22,
+                  10); 
+  } else {
+    this.drawFrame(this.direction, this.frame);
+  }
+
+  if (this.type == ZOMBIE && this.frustration < 100 && Math.random() > 0.95) {
+    this.goTo(player.position);
+  } else {
+    if (!this.stop) {
       switch(this.direction) {
-        case UP:
-          this.direction = DOWN;
+        case DOWN: 
+          this.movement.y = this.speed;
           break;
-        case DOWN:
-          this.direction = UP;
+        case UP:
+          this.movement.y = -this.speed;
           break;
         case LEFT:
-          this.direction = RIGHT;
+          this.movement.x = -this.speed;
           break;
         case RIGHT:
-          this.direction = LEFT;
+          this.movement.x = this.speed
           break;
       }
-    } else {
-      this.stop = true;
     }
+  }
+  this.position.add(this.movement);
 
-    this.xPos = this.xPosOld;
-    this.yPos = this.yPosOld;
-    this.position.x = this.oldPosition.x;
-    this.position.y = this.oldPosition.y;
-  };
-
-  /* Draw a single frame */
-  this.drawFrame = function(row, col) {
-    if (row >= 0) {
-      ctx.drawImage(this.image, 
-                    this.width * col,
-                    row * this.height,
-                    this.width,
-                    this.height,
-                    quickRound(this.position.x),
-                    quickRound(this.position.y),
-                    this.width,
-                    this.height); 
-    } else {
-      ctx.save();
-      ctx.scale(-1,1);
-      row = -row;
-      ctx.drawImage(this.image,
-                    this.width * col,
-                    row * this.height,
-                    this.width,
-                    this.height,
-                    -quickRound(this.position.x) - this.width,
-                    quickRound(this.position.y),
-                    this.width,
-                    this.height); 
-      ctx.restore();
-    }
-  };
-
-  /* Gets called once every frame */
-  this.nextFrame = function() {
-
-    this.xPosOld = this.xPos;
-    this.yPosOld = this.yPos;
-
-    this.oldPosition.x = this.position.x;
-    this.oldPosition.y = this.position.y;
-
-    this.movement.x = 0;
-    this.movement.y = 0;
-
-    if (this.type == DEAD) {
-      //temporary, will be part of sprite
-      var img = new Image();
-      img.src = "dead-zombie.png";
-      ctx.drawImage(img, 
-                    0,
-                    0,
-                    22,
-                    10,
-                    this.position.x,
-                    this.position.y + 10,
-                    22,
-                    10); 
-    } else {
-      this.drawFrame(this.direction, this.frame);
-    }
-
-    if (this.type == ZOMBIE && this.frustration < 100 && Math.random() > 0.95) {
-      this.goTo(player.position);
-    } else {
-      if (!this.stop) {
-        switch(this.direction) {
-          case DOWN: 
-            this.movement.y = this.speed;
-            break;
-          case UP:
-            this.movement.y = -this.speed;
-            break;
-          case LEFT:
-            this.movement.x = -this.speed;
-            break;
-          case RIGHT:
-            this.movement.x = this.speed
-            break;
-        }
-      }
-    }
-    this.position.add(this.movement);
-
-    if (this.stop) {
+  if (this.stop) {
+    this.frame = 0;
+  } else {
+    this.frame++;
+    if (this.frame == this.totalframes)
       this.frame = 0;
-    } else {
-      this.frame++;
-      if (this.frame == this.totalframes)
-        this.frame = 0;
-    }
-
-  };
-};
+  }
+}
 
 $(document).keypress(function(e) {
   if (e.keyCode != 32 || ammo < 1)
